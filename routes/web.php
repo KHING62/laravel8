87bf148bcdf99de1ab353abc4543b65c05742907
KHING62 <khingg.tobundit@gmail.com>
2023-06-27 16:02:22 +0700

    yes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/homepage", function () {
+    return "<h1>This is home page</h1>";
+});
+
+Route::get("/blog/{id}/edit", function ($id) {
+    return "<h1>This is blog page : {$id} for edit</h1>";
+});
+
+Route::get("/product/{a}/{b}/{c}", function ($a, $b, $c) {
+    return "<h1>This is product page </h1><div>{$a} , {$b}, {$c}</div>";
+});
+Route::get("/hello", function () {
+    return view("hello");
+});
+Route::get('/greeting', function () {
+
+    $name = 'khing';
+    $last_name = 'Mars';
+
+    return view('greeting', compact('name', 'last_name'));
+});
