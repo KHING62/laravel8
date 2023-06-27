f030085cfce204dfca74d9591a90a6e9715cc7f2
KHING62 <khingg.tobundit@gmail.com>
2023-06-27 17:25:52 +0700

    week 02

---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <h2>HTML Image</h2>
+    <img src=" {{ $ant}} " alt="Trulli" width="300" height="330">
+
+    <img src="{{$bird}}" alt="Trulli" width="300" height="330">
+
+    <img src="{{$cat}}" alt="Trulli" width="300" height="330">
+
+    <img src="{{$god}}" alt="Trulli" width="300" height="330">
+
+    <img src="{{$spider}}" alt="Trulli" width="300" height="330">
+</body>
+
+</html>
