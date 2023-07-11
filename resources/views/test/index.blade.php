a87ae0983dd5d4edfb2029562098bd4ca214b182
KHING62 <khingg.tobundit@gmail.com>
2023-07-11 16:35:41 +0700

    Week4

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -3,16 +3,16 @@
 
 <body>
 
-    <h2>HTML Image</h2>
-    <a href="gallery/ant" ><img src=" {{ $ant}} " alt="Trulli" width="300" height="330"></a>
+    <h2>Pongsakom Tobundit </h2>
+    <a href="newgallery/ant" ><img src=" {{ $ant}} " alt="Trulli" width="300" height="330"></a>
 
-    <a href="gallery/bird" ><img src="{{$bird}}" alt="Trulli" width="300" height="330"></a>
+    <a href="newgallery/bird" ><img src="{{$bird}}" alt="Trulli" width="300" height="330"></a>
 
-    <a href="gallery/cat" ><img src="{{$cat}}" alt="Trulli" width="300" height="330"></a>
+    <a href="newgallery/cat" ><img src="{{$cat}}" alt="Trulli" width="300" height="330"></a>
 
-    <a href="gallery/ant" ><img src="{{$god}}" alt="Trulli" width="300" height="330"></a>
+    <a href="newgallery/ant" ><img src="{{$god}}" alt="Trulli" width="300" height="330"></a>
 
-    <a href="gallery/ant" ><img src="{{$spider}}" alt="Trulli" width="300" height="330"></a>
+    <a href="newgallery/ant" ><img src="{{$spider}}" alt="Trulli" width="300" height="330"></a>
 </body>
 
 </html>
